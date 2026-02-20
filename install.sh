1c39da5cb34094fb031d8f2cb46a9ba34c941db7
zlc1004 <lucaszhang1001@gmail.co>
2026-02-19 20:01:32 -0800

    feat(install): add pv package to installation script for enhanced progress visualization feat(upload): modify rsync command to include additional options for improved file transfer

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/zlc1004/VeOmni.git
 apt update
-apt install -y ffmpeg rsync
+apt install -y ffmpeg rsync pv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 cd VeOmni
