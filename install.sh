8ddebe229666cbabbc8454151558bf6b4bc52725
zlc1004 <lucaszhang1001@gmail.co>
2026-02-19 19:46:00 -0800

    feat(install): add source and update commands for VeOmni setup

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ git clone https://github.com/zlc1004/VeOmni.git
 apt update
 apt install -y ffmpeg
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 cd VeOmni
+uv self update 0.9.8
 uv sync --locked  --extra gpu --extra video
