6b45b9347cd599d1a7754cb26467a644abf454b6
zlc1004 <lucaszhang1001@gmail.co>
2026-02-19 20:44:27 -0800

    fix(install): specify full path for hf download command in installation script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt install -y ffmpeg rsync pv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 curl -LsSf https://hf.co/cli/install.sh | bash
 source $HOME/.local/bin/env
-hf download Qwen/Qwen2-VL-7B --local-dir ./models/Qwen2-VL-7B-Base
+/root/.local/bin/hf download Qwen/Qwen2-VL-7B --local-dir ./models/Qwen2-VL-7B-Base
 cd VeOmni
 uv self update 0.9.8
 uv sync --locked  --extra gpu --extra video
